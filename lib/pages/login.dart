543eb9b3df7dc8403ac5efa181eb13f823f429cf
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-09-06 20:26:47 +0530

    altered button size and used scrollview

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -6,54 +6,58 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Column(
-        children: [
-          Image.asset(
-            '/images/login.png',
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            'Welcome to Login page',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              '/images/login.png',
+              fit: BoxFit.cover,
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter name',
-                    labelText: 'Username',
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: 'Enter password',
-                    labelText: 'password',
-                  ),
-                ),
-              ],
+            SizedBox(
+              height: 20,
             ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          ElevatedButton(
+            Text(
+              'Welcome to Login page',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter name',
+                      labelText: 'Username',
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Enter password',
+                      labelText: 'password',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/');
               },
-              child: Text('Login!!')),
-        ],
+              child: Text('Login!!'),
+              style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+            ),
+          ],
+        ),
       ),
     );
   }
