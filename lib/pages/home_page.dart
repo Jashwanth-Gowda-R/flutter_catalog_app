5a157032c747e0a1b1f8638582dbf28922aa0adc
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2021-09-08 15:30:34 +0530

    local files and tried load and decode json

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,24 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_ecommerce_app/models/catalog.dart';
 import 'package:flutter_ecommerce_app/widgets/drawer.dart';
 import 'package:flutter_ecommerce_app/widgets/item_widget.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,5 +38,14 @@ class Home extends StatelessWidget {
                 );
               }),
         ));
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString('/files/catalog.json');
+    // print(catalogJson);
+    var data = jsonDecode(catalogJson);
+    // print(data);
+    var productsData = data['products'];
+    print(productsData);
   }
 }
